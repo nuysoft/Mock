29ae723f3eaf7df4ecb05faee2dee95011b2a339
墨智 <mozhi.gyy@taobao.com>
2014-08-26 15:05:21 +0800

    在 node 环境下初始化 window 为 {}

--- src/fix/prefix-2.js
+++ src/fix/prefix-2.js
@@ -1,9 +1,15 @@
+    try {
+    	// for node
+    	window
+    } catch (error) {
+    	window = {}
+    }
     expose(
-        'mockjs', [],
-        factory,
-        function() {
-            // Browser globals
-            window.Mock = factory()
-            window.Random = Mock.Random
-        }
+    	'mockjs', [],
+    	factory,
+    	function() {
+    		// Browser globals
+    		window.Mock = factory()
+    		window.Random = Mock.Random
+    	}
     )
