cec3cd28a603a97fbc246f29a78954e681861a40
mozhi.gyy <mozhi.gyy@alibaba-inc.com>
2015-12-16 12:15:21 +0800

    + 1.0.0-beta2

--- dist/mock.js
+++ dist/mock.js
@@ -85,7 +85,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    _mocked: {}
 	}
 
-	Mock.version = '1.0.0-beta1'
+	Mock.version = '1.0.0-beta2'
 
 	// 避免循环依赖
 	if (XHR) XHR.Mock = Mock
