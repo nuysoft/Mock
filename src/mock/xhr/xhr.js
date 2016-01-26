f1b723e4c33a12b4a9e6e4d36a55391c1b997621
Archer_小A <qddegtya@163.com>
2016-01-26 14:30:28 +0800

    fix #92

--- src/mock/xhr/xhr.js
+++ src/mock/xhr/xhr.js
@@ -12,7 +12,7 @@
     6. 兼容 XMLHttpRequest 和 ActiveXObject
         new window.XMLHttpRequest()
         new window.ActiveXObject("Microsoft.XMLHTTP")
-    
+
     关键方法的逻辑：
     * new   此时尚无法确定是否需要拦截，所以创建原生 XHR 对象是必须的。
     * open  此时可以取到 URL，可以决定是否进行拦截。
@@ -21,7 +21,7 @@
     规范：
     http://xhr.spec.whatwg.org/
     http://www.w3.org/TR/XMLHttpRequest2/
-    
+
     参考实现：
     https://github.com/philikon/MockHttpRequest/blob/master/lib/mock.js
     https://github.com/trek/FakeXMLHttpRequest/blob/master/fake_xml_http_request.js
@@ -278,7 +278,7 @@ define(['mock/util'], function(Util) {
 
                 that.status = 200
                 that.statusText = HTTP_STATUS_CODES[200]
-                that.responseText = JSON.stringify(
+                that.response = that.responseText = JSON.stringify(
                     convert(that.custom.template, that.custom.options),
                     null, 4
                 )
@@ -338,7 +338,7 @@ define(['mock/util'], function(Util) {
             return headers
         },
         overrideMimeType: function( /*mime*/ ) {},
-        responseType: '', // '', 'text', 'arraybuffer', 'blob', 'document', 'json' 
+        responseType: '', // '', 'text', 'arraybuffer', 'blob', 'document', 'json'
         response: null,
         responseText: '',
         responseXML: null
