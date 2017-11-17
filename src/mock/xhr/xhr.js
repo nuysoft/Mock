a0a78a69fa17a6d619ef89fc281ca5e03733847d
LGGGGG <384652119@qq.com>
2017-11-17 17:16:51 +0800

    Update xhr.js
    解决request.upload.addListener监听事件（监听上传进度）

--- src/mock/xhr/xhr.js
+++ src/mock/xhr/xhr.js
@@ -82,7 +82,7 @@ var XHR_STATES = {
 
 var XHR_EVENTS = 'readystatechange loadstart progress abort error load timeout loadend'.split(' ')
 var XHR_REQUEST_PROPERTIES = 'timeout withCredentials'.split(' ')
-var XHR_RESPONSE_PROPERTIES = 'readyState responseURL status statusText responseType response responseText responseXML'.split(' ')
+var XHR_RESPONSE_PROPERTIES = 'readyState responseURL status statusText responseType response responseText responseXML upload'.split(' ')
 
 // https://github.com/trek/FakeXMLHttpRequest/blob/master/fake_xml_http_request.js#L32
 var HTTP_STATUS_CODES = {
